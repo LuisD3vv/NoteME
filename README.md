b92609fc46bca8899eed8ef5a5944c78ca756def
Lissandrodev <Luismexico2006@gmail.com>
2026-03-01 22:13:38 -0700

    V4.5.1 - mejorar menores

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Se estara mejorando constantemente
+Se habia hecho un putisimo readme bien perro pero se elimuino por l mal uso del github
--- static/script.js
+++ static/script.js
@@ -1,9 +1,24 @@
 let fecha = new Date();
+let notas = []
 
+let orden = document.querySelector("#EnviarOrden")
+orden.addEventListener("click",()=>{
+  console.log("Acomodar")
+})
 
-function CrearElementos(titulo, contenido) {
+function crearNota(titulo, contenido,tag) {
+  // regresar un objeto para ingresarlo como una copia exacta al array
+  return {
+    id: crypto.randomUUID(),   // id único real
+    titulo: titulo,
+    contenido: contenido,
+    fecha: new Date().toISOString(),
+    etiquetas: [tag], // intereseante para luego filtrarla
+  };
+}
+function crearElementos(titulo, contenido) {
   // Crear los elementos traidos del html
-  let elementoContenedor = document.getElementById("contenedor_real");
+  let elementoContenedor = document.querySelector("#contenedor_real");
 
   // CREAR EL ELEMENTO TITULO QUE CONTIENE EL TEXTO EXTRAIDO
   let elementoTitulo = document.createElement("h2");
@@ -16,15 +31,24 @@ function CrearElementos(titulo, contenido) {
   // CREANDO EL ELEMENTO DIV GENERICO QUE ES EL CONTENEDOR DEL TEXTO EXTRAIDO
   let elementoDivContenido = document.createElement("div");
 
+
+  contenido = contenido.replace("\n",""); // Remplazar saltos de linea con espacios vacios
   elementoTitulo.innerText = titulo;
   elementoContenido.innerText = contenido;
+  elementoTag = document.querySelector("#tag").value
+
+  
 
   // DARLE LOS ATRIBUTOS DEL CSS Y SE AJUSTEN SEGUN LOS ESTILOS
   elementosection.setAttribute("id", "nota");
   elementoDivTitulo.setAttribute("class", "titulo_nota");
   elementoDivContenido.setAttribute("class", "contenido_nota");
-  GuardarNota(titulo,contenido)
-  console.log(titulo,contenido)
+
+  //GuardarNota(titulo,contenido)
+  notas.push(crearNota(titulo,contenido,elementoTag));
+
+  localStorage.setItem("notas",JSON.stringify(notas));
+
   //CREANDO LA ESTRUCTURA DE HERENCIA
   elementoDivTitulo.append(elementoTitulo);
   elementoDivContenido.append(elementoContenido);
@@ -36,48 +60,62 @@ function CrearElementos(titulo, contenido) {
 }
 function verificar_notas() {
   // Esta funcion le pasa el contenido de la nota, el titulo y el contenido
-  let titulo_nota = document.getElementById("titulo").value;
-  let contenido_nota = document.getElementById("contenido_nota").value;
+  let titulo_nota = document.querySelector("#titulo").value;
+  let contenido_nota = document.querySelector("#contenido_nota").value;
   if (!titulo_nota && !contenido_nota) {
-    alert("Debes agregar contenido a la nota")
+    alert("Debes agregar contenido a la nota.")
     return
   }
     else if (!titulo_nota) {
-    alert("Ingresa el titulo de la nota");
+    alert("La nota debe tener un titulo.");
     return;
   } else if (!contenido_nota) {
     alert("La nota no puede estar vacia.");
     return;
   } else {
-    CrearElementos(titulo_nota, contenido_nota);
+    crearElementos(titulo_nota, contenido_nota);
   }
   
 }
-
 function ordenamiento(opOrdenamiento) {
   console.log(opOrdenamiento);
 }
+function cargarNotas() {
+  // Aqui tenemos ya el array.
+  let notas = JSON.parse(localStorage.getItem("notas")) || [];
+  let elementoContenedor = document.querySelector("#contenedor_real");
+  
+  console.table(notas)
+  for (let i=0;i<notas.length;i++) {
+  let elementosection = document.createElement("section");
+  let elementoDivTitulo = document.createElement("div");
 
-// El valor del select ya es el valor del option en el estado actual
-// por eso no hace falta llamar a cada option
-let botonOrdenar = document.querySelector("#EnviarOrden")
-botonOrdenar.addEventListener("click",()=>{
-  ordenamiento(document.querySelector("#con_options").value)
-});
+  let elementoDivContenido = document.createElement("div");
+  
+  let elementoTitulo = document.createElement("h2");
 
-// Funcion promesa una peticion
-function GuardarNota(titulo,contenido){
-  fetch("/notes", {
-    method:"POST",
-    headers: {
-      "Content-Type":"application/json"
-    },
-    body:JSON.stringify({titulo, contenido})
-  })
-  // Response vive solo en el primer then()
-  .then(response => response.json())
-  .catch(err=>{
-    console.error(err)
-  });
+  let elementoContenido = document.createElement("p");
+
+
+  elementoTitulo.textContent = notas[i].titulo;
+  elementoContenido.textContent = notas[i].contenido;
+
+    // DARLE LOS ATRIBUTOS DEL CSS Y SE AJUSTEN SEGUN LOS ESTILOS
+  elementosection.setAttribute("id", "nota");
+  elementoDivTitulo.setAttribute("class", "titulo_nota");
+  elementoDivContenido.setAttribute("class", "contenido_nota");
+  
+
+  //CREANDO LA ESTRUCTURA DE HERENCIA
+  elementoDivTitulo.append(elementoTitulo);
+  elementoDivContenido.append(elementoContenido);
+  elementosection.appendChild(elementoDivTitulo);
+  elementosection.appendChild(elementoDivContenido);
+  elementoContenedor.appendChild(elementosection);
+  }
+  
+
+  
 }
 
+cargarNotas()
